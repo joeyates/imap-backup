94e8cf173d5dcf249d045380bb591620742e552a
Joe Yates <joe.g.yates@gmail.com>
2013-01-07 21:30:19 +0100

    Complete 'From_' line

--- spec/unit/email/mboxrd/message_spec.rb
+++ spec/unit/email/mboxrd/message_spec.rb
@@ -4,11 +4,17 @@ require 'spec_helper'
 
 describe Email::Mboxrd::Message do
   let(:from) { 'me@example.com' }
-  let(:message_body) { "Foo\nBar\nFrom at the beginning of the line" }
-  subject { Email::Mbox::Message.new(message_body) }
+  let(:date) { DateTime.new(2012, 12, 13, 18, 23, 45) }
+  let(:message_body) { "Foo\nBar\nFrom at the beginning of the line\n>>From quoted" }
+  subject { Email::Mboxrd::Message.new(message_body) }
 
-  context '#to_mbox' do
-    let(:mail) { stub('Mail') }
+  context '#to_s' do
+    let(:mail) do
+      mail = stub('Mail')
+      mail.stub(:from).and_return([from])
+      mail.stub(:date).and_return(date)
+      mail
+    end
 
     before do
       Mail.stub(:new).with(message_body).and_return(mail)
@@ -18,15 +24,19 @@ describe Email::Mboxrd::Message do
     it 'parses the message' do
       Mail.should_receive(:new).with(message_body).and_return(mail)
 
-      subject.to_mbox
+      subject.to_s
     end
 
     it "adds a 'From ' line at the start" do
-      expect(subject.to_mbox).to start_with('From ' + from + "\n")
+      expect(subject.to_s).to start_with('From ' + from + ' ' + date.asctime + "\n")
     end
 
     it "replaces existing 'From ' with '>From '" do
-      expect(subject.to_mbox).to include("\n>From at the beginning")
+      expect(subject.to_s).to include("\n>From at the beginning")
+    end
+
+    it "appends > before '>+From '" do
+      expect(subject.to_s).to include("\n>>>From quoted")
     end
   end
 end
