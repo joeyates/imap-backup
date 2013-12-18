5b3f4b0348f73206f8cd3ae25d294ae87331b7fd
Joe Yates <joe.g.yates@gmail.com>
2013-12-18 18:45:59 +0100

    Add a logging singleton

--- lib/imap/backup.rb
+++ lib/imap/backup.rb
@@ -14,6 +14,22 @@ require 'imap/backup/serializer/directory'
 require 'imap/backup/serializer/mbox'
 require 'imap/backup/version'
 
+require 'logger'
+
 module Imap::Backup
   class ConfigurationNotFound < StandardError; end
+
+  class Logger
+    include Singleton
+
+    attr_reader :logger
+
+    def initialize
+      @logger = ::Logger.new(STDOUT)
+    end
+  end
+
+  def self.logger
+    Imap::Backup::Logger.instance.logger
+  end
 end
