d5a83b07d89e68e76b0c5bc3f1fe355afeae23ab
Joe Yates <joe.g.yates@gmail.com>
2014-05-15 16:26:33 +0200

    Clarify difference between bodies

--- lib/email/mboxrd/message.rb
+++ lib/email/mboxrd/message.rb
@@ -4,29 +4,32 @@ module Email; end
 
 module Email::Mboxrd
   class Message
-    def initialize(body)
-      @body = body.clone
-      @body.force_encoding('binary') if RUBY_VERSION >= '1.9.0'
+    attr_reader :supplied_body
+
+    def initialize(supplied_body)
+      @supplied_body = supplied_body.clone
+      @supplied_body.force_encoding('binary') if RUBY_VERSION >= '1.9.0'
     end
 
     def to_s
-      'From ' + from + "\n" + body + "\n"
+      'From ' + from + "\n" + mboxrd_body + "\n"
     end
 
     private
 
     def parsed
-      @parsed ||= Mail.new(@body)
+      @parsed ||= Mail.new(supplied_body)
     end
 
     def from
       parsed.from[0] + ' ' + asctime
     end
 
-    def body
-      mbox = @body.gsub(/\n(>*From)/, "\n>\\1")
-      mbox += "\n" unless mbox.end_with?("\n")
-      mbox
+    def mboxrd_body
+      return @mboxrd_body if @mboxrd_body
+      @mboxrd_body = supplied_body.gsub(/\n(>*From)/, "\n>\\1")
+      @mboxrd_body += "\n" unless @mboxrd_body.end_with?("\n")
+      @mboxrd_body
     end
 
     def asctime
