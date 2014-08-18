c036fce5238373f53df472e71e5080f9ba62880c
Joe Yates <joe.g.yates@gmail.com>
2014-08-18 13:32:59 +0200

    Use delegation

--- lib/imap/backup/account/folder.rb
+++ lib/imap/backup/account/folder.rb
@@ -1,29 +1,34 @@
 # encoding: utf-8
+require 'forwardable'
 
 module Imap::Backup
   module Account; end
 
   class Account::Folder
+    extend Forwardable
+
     REQUESTED_ATTRIBUTES = ['RFC822', 'FLAGS', 'INTERNALDATE']
 
     attr_reader :connection
     attr_reader :folder
+
+    delegate imap: :connection
 
     def initialize(connection, folder)
       @connection, @folder = connection, folder
     end
 
     def uids
-      connection.imap.examine(folder)
-      connection.imap.uid_search(['ALL']).sort
+      imap.examine(folder)
+      imap.uid_search(['ALL']).sort
     rescue Net::IMAP::NoResponseError => e
       Imap::Backup.logger.warn "Folder '#{folder}' does not exist"
       []
     end
 
     def fetch(uid)
-      connection.imap.examine(folder)
-      message = connection.imap.uid_fetch([uid.to_i], REQUESTED_ATTRIBUTES)[0][1]
+      imap.examine(folder)
+      message = imap.uid_fetch([uid.to_i], REQUESTED_ATTRIBUTES)[0][1]
       message['RFC822'].force_encoding('utf-8') if RUBY_VERSION > '1.9'
       message
     rescue Net::IMAP::NoResponseError => e
