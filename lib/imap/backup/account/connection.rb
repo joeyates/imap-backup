60d39158ef6bb9eb68871a7cfca0d4be14ca067d
Joe Yates <joe.g.yates@gmail.com>
2014-08-18 15:06:10 +0200

    Make method private

--- lib/imap/backup/account/connection.rb
+++ lib/imap/backup/account/connection.rb
@@ -50,12 +50,6 @@ module Imap::Backup
       imap.disconnect
     end
 
-    def server
-      return @server if @server
-      return nil if provider.nil?
-      @server = provider.host
-    end
-
     def imap
       return @imap unless @imap.nil?
       options = provider_options
@@ -84,6 +78,12 @@ module Imap::Backup
 
     def provider
       @provider ||= Email::Provider.for_address(username)
+    end
+
+    def server
+      return @server if @server
+      return nil if provider.nil?
+      @server = provider.host
     end
 
     def provider_options
