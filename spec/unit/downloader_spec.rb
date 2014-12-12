66eb975288af86bee79aba71ef6931b1b1af5149
Joe Yates <joe.g.yates@gmail.com>
2014-12-12 15:04:27 +0100

    Clean up spec

--- spec/unit/downloader_spec.rb
+++ spec/unit/downloader_spec.rb
@@ -2,76 +2,38 @@
 require 'spec_helper'
 
 describe Imap::Backup::Downloader do
-  context 'with account and downloader' do
-    let(:local_path) { '/base/path' }
-    let(:stat) { double('File::Stat', :mode => 0700) }
-    let(:message) do
-      {
-        'RFC822' => 'the body',
-        'other'  => 'xxx'
-      }
-    end
+  describe '#run' do
+    let(:message) { 'message' }
     let(:folder) { double('Imap::Backup::Account::Folder', :fetch => message) }
-    let(:serializer) do
-      double(
-        'Imap::Backup::Serializer',
-        :prepare => nil,
-        :exist?  => true,
-        :uids    => [],
-        :save    => nil,
-      )
-    end
-
-    before { allow(File).to receive(:stat).with(local_path).and_return(stat) }
+    let(:folder_uids) { ['111', '222', '333'] }
+    let(:serializer) { double('Imap::Backup::Serializer', :save => nil) }
+    let(:serializer_uids) { ['222'] }
 
     subject { described_class.new(folder, serializer) }
 
+    before do
+      allow(folder).to receive(:uids).and_return(folder_uids)
+      allow(serializer).to receive(:uids).and_return(serializer_uids)
+      allow(folder).to receive(:fetch).with('333').and_return(nil)
+      subject.run
+    end
+
     context '#run' do
-      context 'with folder' do
-        it 'should list messages' do
-          allow(folder).to receive(:uids).and_return([])
-
-          subject.run
+      context 'fetched messages' do
+        it 'are saved' do
+          expect(serializer).to have_received(:save).with('111', message)
         end
+      end
 
-        context 'with messages' do
-          before :each do
-            allow(folder).to receive(:uids).and_return(['123', '999', '1234'])
-          end
+      context 'messages which are already present' do
+        specify 'are skipped' do
+          expect(serializer).to_not have_received(:save).with('222', anything)
+        end
+      end
 
-          it 'skips failed fetches' do
-            allow(folder).to receive(:fetch).with('999').and_return(nil)
-
-            subject.run
-
-            expect(serializer).to_not have_received(:save).with('999', anything)
-          end
-
-          context 'to download' do
-            before :each do
-              allow(serializer).to receive(:exist?) do |uid|
-                if uid == '123'
-                  true
-                else
-                  false
-                end
-              end
-            end
-
-            it 'requests messages' do
-              subject.run
-
-              expect(folder).to have_received(:fetch).with('999')
-              expect(folder).to have_received(:fetch).with('1234')
-            end
-
-            it 'saves messages' do
-              subject.run
-
-              expect(serializer).to have_received(:save).with('999', message)
-              expect(serializer).to have_received(:save).with('1234', message)
-            end
-          end
+      context 'failed fetches' do
+        specify 'are skipped' do
+          expect(serializer).to_not have_received(:save).with('333', anything)
         end
       end
     end
