d2e1fb0a33b4a5b84a6527def6af3880a2ddc4d5
Joe Yates <joe.g.yates@gmail.com>
2017-08-20 10:58:38 +0200

    Allow gTLDs

--- lib/imap/backup/configuration/asker.rb
+++ lib/imap/backup/configuration/asker.rb
@@ -4,7 +4,7 @@ module Imap::Backup
   module Configuration; end
 
   class Configuration::Asker < Struct.new(:highline)
-    EMAIL_MATCHER = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+    EMAIL_MATCHER = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]+$/i
 
     def initialize(highline)
       super
