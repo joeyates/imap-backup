c8d73720ef0f7335ff288ee35590e68ed9891aca
Joe Yates <joe.g.yates@gmail.com>
2018-12-03 22:55:11 +0100

    Add coverage to Connection

--- spec/unit/imap/backup/account/connection_spec.rb
+++ spec/unit/imap/backup/account/connection_spec.rb
@@ -28,9 +28,21 @@ describe Imap::Backup::Account::Connection do
     instance_double(Net::IMAP::MailboxList, name: root_name)
   end
   let(:root_name) { "foo" }
+  let(:serializer) do
+    instance_double(
+      Imap::Backup::Serializer::Mbox,
+      folder: serialized_folder,
+      force_uid_validity: nil,
+      set_uid_validity: new_uid_validity,
+      uids: [local_uid]
+    )
+  end
+  let(:serialized_folder) { nil }
+  let(:new_uid_validity) { nil }
+  let(:local_uid) { "local_uid" }
 
   before do
-    allow(Net::IMAP).to receive(:new).and_return(imap)
+    allow(Net::IMAP).to receive(:new) { imap }
     allow(imap).to receive(:list).with("", "") { [root_info] }
     allow(imap).to receive(:list).with(root_name, "*") { imap_folders }
     allow(Imap::Backup::Utils).to receive(:make_folder)
@@ -89,10 +101,6 @@ describe Imap::Backup::Account::Connection do
     let(:folder) do
       instance_double(Imap::Backup::Account::Folder, uids: [remote_uid])
     end
-    let(:local_uid) { "local_uid" }
-    let(:serializer) do
-      instance_double(Imap::Backup::Serializer::Mbox, uids: [local_uid])
-    end
     let(:remote_uid) { "remote_uid" }
 
     before do
@@ -122,12 +130,6 @@ describe Imap::Backup::Account::Connection do
       )
     end
     let(:uid_validity) { 123 }
-    let(:serializer) do
-      instance_double(
-        Imap::Backup::Serializer::Mbox,
-        set_uid_validity: nil
-      )
-    end
     let(:downloader) { instance_double(Imap::Backup::Downloader, run: nil) }
 
     before do
@@ -193,13 +195,120 @@ describe Imap::Backup::Account::Connection do
     end
   end
 
+  context "#restore" do
+    let(:folder) do
+      instance_double(
+        Imap::Backup::Account::Folder,
+        create: nil,
+        exist?: exists,
+        name: "my_folder",
+        uid_validity: uid_validity
+      )
+    end
+    let(:exists) { true }
+    let(:uid_validity) { 123 }
+    let(:serialized_folder) { "old name" }
+    let(:uploader) do
+      instance_double(Imap::Backup::Uploader, run: false)
+    end
+    let(:updated_uploader) do
+      instance_double(Imap::Backup::Uploader, run: false)
+    end
+    let(:updated_folder) do
+      instance_double(
+        Imap::Backup::Account::Folder,
+        create: nil,
+        uid_validity: "new uid validity"
+      )
+    end
+    let(:updated_serializer) do
+      instance_double(
+        Imap::Backup::Serializer::Mbox, force_uid_validity: nil
+      )
+    end
+
+    before do
+      allow(Imap::Backup::Account::Folder).to receive(:new).
+        with(subject, "my_folder") { folder }
+      allow(Imap::Backup::Serializer::Mbox).to receive(:new).
+        with(anything, "my_folder") { serializer }
+      allow(Imap::Backup::Account::Folder).to receive(:new).
+        with(subject, "new name") { updated_folder }
+      allow(Imap::Backup::Serializer::Mbox).to receive(:new).
+        with(anything, "new name") { updated_serializer }
+      allow(Imap::Backup::Uploader).to receive(:new).
+        with(folder, serializer) { uploader }
+      allow(Imap::Backup::Uploader).to receive(:new).
+        with(updated_folder, updated_serializer) { updated_uploader }
+      allow(Pathname).to receive(:glob).
+        and_yield(Pathname.new(File.join(local_path, "my_folder.imap")))
+      subject.restore
+    end
+
+    it "sets local uid validity" do
+      expect(serializer).to have_received(:set_uid_validity).with(uid_validity)
+    end
+
+    context "when folders exist" do
+      context "when the local folder is renamed" do
+        let(:new_uid_validity) { "new name" }
+
+        it "creates the new folder" do
+          expect(updated_folder).to have_received(:create)
+        end
+
+        it "sets the renamed folder's uid validity" do
+          expect(updated_serializer).
+            to have_received(:force_uid_validity).with("new uid validity")
+        end
+
+        it "creates the uploader with updated folder and serializer" do
+          expect(updated_uploader).to have_received(:run)
+        end
+      end
+
+      context "when the local folder is not renamed" do
+        it "runs the uploader" do
+          expect(uploader).to have_received(:run)
+        end
+      end
+    end
+
+    context "when folders don't exist" do
+      let(:exists) { false }
+
+      it "creates the folder" do
+        expect(folder).to have_received(:create)
+      end
+
+      it "sets local uid validity" do
+      end
+
+      it "runs the uploader" do
+        expect(uploader).to have_received(:run)
+      end
+    end
+  end
+
+  context "#reconnect" do
+    before { subject.reconnect }
+
+    it "disconnects from the server" do
+      expect(imap).to have_received(:disconnect)
+    end
+
+    it "causes reconnection on future access" do
+      allow(Net::IMAP).to receive(:new) { imap }
+      subject.imap
+      expect(Net::IMAP).to have_received(:new).twice
+    end
+  end
+
   context "#disconnect" do
     before { subject.disconnect }
 
     it "disconnects from the server" do
       expect(imap).to have_received(:disconnect)
     end
-
-    include_examples "connects to IMAP"
   end
 end
