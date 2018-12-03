46b8b8762a285d4e60e5d2274d6903886a0d9a7b
Joe Yates <joe.g.yates@gmail.com>
2018-12-03 21:39:16 +0100

    Add coverage for Mbox#set_uid_validity

--- spec/unit/imap/backup/serializer/mbox_spec.rb
+++ spec/unit/imap/backup/serializer/mbox_spec.rb
@@ -4,12 +4,16 @@ describe Imap::Backup::Serializer::Mbox do
     instance_double(
       Imap::Backup::Serializer::MboxStore,
       add: nil,
-      uids: nil
+      rename: nil,
+      uids: nil,
+      uid_validity: existing_uid_validity,
+      "uid_validity=": nil
     )
   end
   let(:imap_folder) { "folder" }
   let(:permissions) { 0o700 }
   let(:dir_exists) { true }
+  let(:existing_uid_validity) { nil }
 
   before do
     allow(Imap::Backup::Utils).to receive(:make_folder)
@@ -77,6 +81,84 @@ describe Imap::Backup::Serializer::Mbox do
       subject.save("foo", "bar")
 
       expect(store).to have_received(:add)
+    end
+  end
+
+  context "#set_uid_validity" do
+    let(:result) { subject.set_uid_validity("aaa") }
+
+    context "when the existing uid validity is unset" do
+      let!(:result) { super() }
+
+      it "sets uid validity" do
+        expect(store).to have_received(:uid_validity=).with("aaa")
+      end
+
+      it "does not rename the store" do
+        expect(store).to_not have_received(:rename)
+      end
+
+      it "returns nil" do
+        expect(result).to be_nil
+      end
+    end
+
+    context "when the uid validity is unchanged" do
+      let!(:result) { super() }
+      let(:existing_uid_validity) { "aaa" }
+
+      it "does not set uid validity" do
+        expect(store).to_not have_received(:uid_validity=)
+      end
+
+      it "does not rename the store" do
+        expect(store).to_not have_received(:rename)
+      end
+
+      it "returns nil" do
+        expect(result).to be_nil
+      end
+    end
+
+    context "when the uid validity is changed" do
+      let(:existing_uid_validity) { "bbb" }
+      let(:existing_store) do
+        instance_double(Imap::Backup::Serializer::MboxStore)
+      end
+      let(:exists) { false }
+
+      before do
+        allow(Imap::Backup::Serializer::MboxStore).
+          to receive(:new).with(anything, /bbb/) { existing_store }
+        allow(existing_store).to receive(:exist?).and_return(exists, false)
+        result
+      end
+
+      it "sets uid validity" do
+        expect(store).to have_received(:uid_validity=).with("aaa")
+      end
+
+      context "when adding the uid validity does not cause a name clash" do
+        it "renames the store, adding the existing uid validity" do
+          expect(store).to have_received(:rename).with("folder.bbb")
+        end
+
+        it "returns the new name" do
+          expect(result).to eq("folder.bbb")
+        end
+      end
+
+      context "when adding the uid validity causes a name clash" do
+        let(:exists) { true }
+
+        it "renames the store, adding the existing uid validity and a digit" do
+          expect(store).to have_received(:rename).with("folder.bbb.1")
+        end
+
+        it "returns the new name" do
+          expect(result).to eq("folder.bbb.1")
+        end
+      end
     end
   end
 end
