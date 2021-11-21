ffced7320c24461236377f8cf62040c3e837ab81
Joe Yates <joe.g.yates@gmail.com>
2021-11-21 11:35:28 +0100

    Bump release candidate

--- lib/imap/backup/version.rb
+++ lib/imap/backup/version.rb
@@ -4,6 +4,6 @@ module Imap::Backup
   MAJOR    = 4
   MINOR    = 0
   REVISION = 0
-  PRE      = "rc5"
+  PRE      = "rc6"
   VERSION  = [MAJOR, MINOR, REVISION, PRE].compact.map(&:to_s).join(".")
 end
