81cae5dbd5400b3bab9b709140d030236e439f61
Joe Yates <joe.g.yates@gmail.com>
2021-11-22 11:20:36 +0100

    Release version 4

--- lib/imap/backup/version.rb
+++ lib/imap/backup/version.rb
@@ -4,6 +4,6 @@ module Imap::Backup
   MAJOR    = 4
   MINOR    = 0
   REVISION = 0
-  PRE      = "rc6"
+  PRE      = nil
   VERSION  = [MAJOR, MINOR, REVISION, PRE].compact.map(&:to_s).join(".")
 end
