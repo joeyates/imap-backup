2a828ff852d8eeecce4a072adcfb8ea3b7339bb5
Joe Yates <joe.g.yates@gmail.com>
2022-01-10 15:19:25 +0100

    Bump

--- lib/imap/backup/version.rb
+++ lib/imap/backup/version.rb
@@ -2,8 +2,8 @@ module Imap; end
 
 module Imap::Backup
   MAJOR    = 4
-  MINOR    = 1
-  REVISION = 2
+  MINOR    = 2
+  REVISION = 0
   PRE      = nil
   VERSION  = [MAJOR, MINOR, REVISION, PRE].compact.map(&:to_s).join(".")
 end
