0a9b5213b2d5e1b7be29f78bc531b8b96ab52321
Joe Yates <joe.g.yates@gmail.com>
2022-01-10 15:16:58 +0100

    Typo

--- lib/thunderbird/subdirectory.rb
+++ lib/thunderbird/subdirectory.rb
@@ -78,13 +78,13 @@ class Thunderbird::Subdirectory
   def check
     case
     when exists? && !placeholder.exists?
-      Kernel.puts "Can't set up folder '#{folder_path}': '#{full_path}' exists, but '#{placeholder.path}' is missing"
+      Kernel.puts "Can't set up folder '#{full_path}': '#{full_path}' exists, but '#{placeholder.path}' is missing"
       false
     when placeholder.exists? && !placeholder.regular?
-      Kernel.puts "Can't set up folder '#{folder_path}': '#{placeholder.path}' exists, but it is not a regular file"
+      Kernel.puts "Can't set up folder '#{full_path}': '#{placeholder.path}' exists, but it is not a regular file"
       false
     when exists? && !directory?
-      Kernel.puts "Can't set up folder '#{folder_path}': '#{full_path}' exists, but it is not a directory"
+      Kernel.puts "Can't set up folder '#{full_path}': '#{full_path}' exists, but it is not a directory"
       false
     else
       true
