8f31cfb2d3fcf2e4115439d5841f4e316bcfe418
Joe Yates <joe.g.yates@gmail.com>
2022-01-31 13:59:42 +0100

    Handle incomplete responses
    Closes #112

--- lib/imap/backup/downloader.rb
+++ lib/imap/backup/downloader.rb
@@ -31,13 +31,24 @@ module Imap::Backup
         uids_and_bodies.each.with_index do |uid_and_body, j|
           uid = uid_and_body[:uid]
           body = uid_and_body[:body]
-          debug("uid: #{uid} (#{offset + j}/#{count}) - #{body.size} bytes")
-          serializer.save(uid, body)
+          case
+          when !body
+            info("Fetch returned empty body - skipping")
+          when !uid
+            info("Fetch returned empty UID - skipping")
+          else
+            debug("uid: #{uid} (#{offset + j}/#{count}) - #{body.size} bytes")
+            serializer.save(uid, body)
+          end
         end
       end
     end
 
     private
+
+    def info(message)
+      Imap::Backup::Logger.logger.info("[#{folder.name}] #{message}")
+    end
 
     def debug(message)
       Imap::Backup::Logger.logger.debug("[#{folder.name}] #{message}")
