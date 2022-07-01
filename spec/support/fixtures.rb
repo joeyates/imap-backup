9dbf04c399ce94fbbe39fd507ac842969e53ecb6
Joe Yates <joe.g.yates@gmail.com>
2022-07-01 19:37:52 +0200

    Call YAML.safe_load differently according to required parameters

--- spec/support/fixtures.rb
+++ spec/support/fixtures.rb
@@ -7,5 +7,11 @@ def fixture(name)
   content = File.read(fixture_path)
   template = ERB.new(content)
   yaml = template.result(binding)
-  YAML.safe_load(yaml, permitted_classes: [Symbol])
+  parameters = YAML.method(:safe_load).parameters
+  has_permitted_classes = parameters.find { |(type, name)| name == :permitted_classes }
+  if has_permitted_classes
+    YAML.safe_load(yaml, permitted_classes: [Symbol])
+  else
+    YAML.safe_load(yaml, [Symbol])
+  end
 end
