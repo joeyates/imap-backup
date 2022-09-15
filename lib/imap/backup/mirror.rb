808364b2ccca56c60571a9126ece4bc028863ae1
Joe Yates <joe.g.yates@gmail.com>
2022-09-15 13:09:18 +0200

    Delete destination only emails

--- lib/imap/backup/mirror.rb
+++ lib/imap/backup/mirror.rb
@@ -12,6 +12,7 @@ module Imap::Backup
 
     def run
       ensure_destination_folder
+      delete_destination_only_emails
     end
 
     private
@@ -20,6 +21,17 @@ module Imap::Backup
       return if folder.exist?
 
       folder.create
+    end
+
+    def delete_destination_only_emails
+      uids_to_delete = destination_only_emails
+      return if uids_to_delete.empty?
+
+      folder.delete_multi(uids_to_delete)
+    end
+
+    def destination_only_emails
+      folder.uids
     end
   end
 end
