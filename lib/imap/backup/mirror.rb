2d8c645737b68633475046a5369db80d31777b1c
Joe Yates <joe.g.yates@gmail.com>
2022-09-15 13:11:40 +0200

    Introduce .mirror file

--- lib/imap/backup/mirror.rb
+++ lib/imap/backup/mirror.rb
@@ -13,6 +13,7 @@ module Imap::Backup
     def run
       ensure_destination_folder
       delete_destination_only_emails
+      map.save
     end
 
     private
@@ -31,7 +32,47 @@ module Imap::Backup
     end
 
     def destination_only_emails
-      folder.uids
+      source_uids = serializer.uids
+      destination_uids = folder.uids
+      destination_uids.filter { |uid| !source_uids.include?(map.source_uid(uid)) }
+    end
+
+    def append_emails
+      serializer.each_message do |message|
+        existing = map.destination_uid(message.uid)
+        next if existing
+
+        # Clients cannot set the 'Recent' flag
+        message.flags.reject! { |f| f == :Recent }
+        destination_uid = folder.append(message)
+        map.map_uids(source: message.uid, destination: destination_uid)
+      end
+    end
+
+    def map
+      @map ||=
+        Mirror::Map.new(pathname: map_pathname, destination: destination_email).tap do |map|
+          map_ok = map.check_uid_validities(
+            source: serializer.uid_validity,
+            destination: folder.uid_validity
+          )
+          if !map_ok
+            folder.clear
+            map.reset(
+              source_uid_validity: serializer.uid_validity,
+              destination_uid_validity: folder.uid_validity
+            )
+          end
+        end
+    end
+
+    def map_pathname
+      "#{serializer.folder_path}.mirror"
+    end
+
+    def destination_email
+      # TODO: is there a more elegant way to get the email?
+      folder.connection.account.username
     end
   end
 end
