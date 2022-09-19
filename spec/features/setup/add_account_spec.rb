e0a6a66b3f5c3c7535dbfa6378ad81edd1dec91a
Joe Yates <joe.g.yates@gmail.com>
2022-09-19 11:56:51 +0200

    Clean up spec

--- spec/features/setup/add_account_spec.rb
+++ spec/features/setup/add_account_spec.rb
@@ -1,32 +1,30 @@
 require "features/helper"
 
 RSpec.describe "adding an account", type: :aruba do
-  let(:home_directory) { File.expand_path("./tmp/home") }
-  let(:imap_directory) { File.join(home_directory, ".imap-backup") }
-  let(:config_path) { File.join(imap_directory, "config.json") }
+  let(:account) { test_server_connection_parameters }
+  let(:config_options) { {accounts: [account]} }
 
   before do
+    create_config **config_options
+
     run_command "imap-backup setup"
     last_command_started.write "add account\n"
-    last_command_started.write "me@example.com\n"
+    last_command_started.write "new@example.com\n"
     last_command_started.write "(q) return to main menu\n"
     last_command_started.write "save and exit\n"
     last_command_started.stop
   end
 
-  it "works" do
-    expect(last_command_started).to have_exit_status(0)
-  end
-
   it "creates the configuration directory" do
-    expect(directory?(imap_directory)).to be true
+    expect(directory?(config_path)).to be true
   end
 
   it "saves account info" do
-    content = read(config_path).join("\n")
+    configuration_file_path = File.join(config_path, "config.json")
+    content = File.read(configuration_file_path)
     config = JSON.parse(content)
-    account = config["accounts"].first
+    account = config["accounts"].last
 
-    expect(account["username"]).to eq("me@example.com")
+    expect(account["username"]).to eq("new@example.com")
   end
 end
