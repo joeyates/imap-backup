bebcd9d47b998f3cca9c7b3cdcbfd521e27aee54
Joe Yates <joe.g.yates@gmail.com>
2022-09-20 10:22:28 +0200

    Split setup feature spec

---
+++ spec/features/setup/configure_account_spec.rb
@@ -0,0 +1,59 @@
+require "features/helper"
+
+RSpec.describe "configuring accounts", type: :aruba do
+  let(:account) { test_server_connection_parameters }
+  let(:email) { account[:username] }
+  let(:config_options) { {accounts: [account]} }
+  let!(:setup) { create_config(**config_options) }
+
+  it "shows account menus" do
+    run_command "imap-backup setup"
+    last_command_started.write "#{email}\n"
+    last_command_started.write "q\n"
+    last_command_started.write "q\n"
+    last_command_started.stop
+
+    expect(last_command_started).to have_output(/imap-backup - Account/m)
+    expect(last_command_started).to have_output(/email\s+#{account[:username]}/)
+  end
+
+  context "when the account's local_path has backslashes" do
+    let(:account) do
+      super().merge(local_path: local_path)
+    end
+    let(:local_path) { "c:\\my_user\\backup" }
+
+    it "works" do
+      run_command "imap-backup setup"
+      last_command_started.write "#{email}\n"
+      last_command_started.write "q\n"
+      last_command_started.write "q\n"
+      last_command_started.stop
+
+      # To match literal 'c:\my_user\backup', we have to escape twice!
+      escaped_local_path = local_path.gsub("\\", "\\\\\\\\")
+
+      expect(last_command_started).to have_output(/path\s+#{escaped_local_path}/)
+      expect(last_command_started).to have_exit_status(0)
+    end
+  end
+
+  context "when the account's connection_options are set" do
+    let(:account) do
+      super().merge(connection_options: connection_options)
+    end
+    let(:connection_options) { {"port" => 600} }
+
+    it "shows them" do
+      run_command "imap-backup setup"
+      last_command_started.write "#{email}\n"
+      last_command_started.write "q\n"
+      last_command_started.write "q\n"
+      last_command_started.stop
+
+      expect(last_command_started).
+        to have_output(/connection options\s+'#{connection_options.to_json}'/)
+      expect(last_command_started).to have_exit_status(0)
+    end
+  end
+end
