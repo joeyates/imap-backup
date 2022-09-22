61de83622bd30f6f33db8a9fd179244f1d7c2142
Joe Yates <joe.g.yates@gmail.com>
2022-09-22 09:16:52 +0200

    Set logger level for 'remote folders'

--- lib/imap/backup/cli/remote.rb
+++ lib/imap/backup/cli/remote.rb
@@ -1,3 +1,5 @@
+require "imap/backup/logger"
+
 module Imap::Backup
   class CLI::Remote < Thor
     include Thor::Actions
@@ -8,6 +10,7 @@ module Imap::Backup
     verbose_option
     quiet_option
     def folders(email)
+      Imap::Backup::Logger.setup_logging options
       config = load_config(**options)
       connection = connection(config, email)
 
