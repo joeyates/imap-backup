077c5ad9a4906f7cb3aa747da084da05385a84c3
Joe Yates <joe.g.yates@gmail.com>
2023-07-08 16:39:42 +0200

    Simplify setup

--- spec/features/local/list_folders_spec.rb
+++ spec/features/local/list_folders_spec.rb
@@ -1,21 +1,15 @@
 require "features/helper"
 
 RSpec.describe "imap-backup local folders", type: :aruba do
-  let(:email) { "me@example.com" }
-  let(:account) do
-    {
-      username: email,
-      local_path: File.join(File.dirname(configuration_path), email.gsub("@", "_"))
-    }
-  end
+  let(:account) { test_server_connection_parameters }
   let(:configuration_path) { File.join(config_path, "config.json") }
   let(:config_options) { {accounts: [account]} }
-  let(:command) { "imap-backup local folders #{email}" }
+  let(:command) { "imap-backup local folders #{account[:username]}" }
 
   before do
     create_config(**config_options)
     append_local(
-      configuration_path: configuration_path, email: email, folder: "my_folder", body: "Hi"
+      configuration_path: configuration_path, email: account[:username], folder: "my_folder", body: "Hi"
     )
 
     run_command_and_stop command
@@ -26,7 +20,7 @@ RSpec.describe "imap-backup local folders", type: :aruba do
   end
 
   context "when JSON is requested" do
-    let(:command) { "imap-backup local folders #{email} --format json" }
+    let(:command) { "imap-backup local folders #{account[:username]} --format json" }
 
     it "lists folders as JSON" do
       expect(last_command_started).to have_output(/\{"name":"my_folder"\}/)
@@ -34,11 +28,10 @@ RSpec.describe "imap-backup local folders", type: :aruba do
   end
 
   context "when a config path is supplied" do
-    let(:email) { "other@example.com" }
     let(:custom_config_path) { File.join(File.expand_path("~/.imap-backup"), "foo.json") }
     let(:configuration_path) { custom_config_path }
     let(:config_options) { {path: custom_config_path, accounts: [account]} }
-    let(:command) { "imap-backup local folders #{email} --config #{custom_config_path}" }
+    let(:command) { "imap-backup local folders #{account[:username]} --config #{custom_config_path}" }
 
     it "lists folders" do
       expect(last_command_started).to have_output('"my_folder"')
