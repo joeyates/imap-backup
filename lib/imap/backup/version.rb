c09a3bdc92073d711f2c0596069126aa9339304b
Joe Yates <joe.g.yates@gmail.com>
2023-07-18 16:47:08 +0200

    Prepare pre-release

--- lib/imap/backup/version.rb
+++ lib/imap/backup/version.rb
@@ -1,9 +1,9 @@
 module Imap; end
 
 module Imap::Backup
-  MAJOR    = 10
+  MAJOR    = 11
   MINOR    = 0
-  REVISION = 1
-  PRE      = nil
+  REVISION = 0
+  PRE      = "rc1".freeze
   VERSION  = [MAJOR, MINOR, REVISION, PRE].compact.map(&:to_s).join(".")
 end
