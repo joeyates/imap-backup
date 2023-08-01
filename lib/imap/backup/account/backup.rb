b60bad772943638372eb666ee3c9327e13629de1
Joe Yates <joe.g.yates@gmail.com>
2023-08-01 09:26:37 +0200

    Extract method

--- lib/imap/backup/account/backup.rb
+++ lib/imap/backup/account/backup.rb
@@ -27,67 +27,69 @@ module Imap::Backup
 
       Account::FolderEnsurer.new(account: account).run
       Account::LocalOnlyFolderDeleter.new(account: account).run if account.mirror_mode
-      each_folder do |folder, serializer|
-        begin
-          next if !folder.exist?
-        rescue Encoding::UndefinedConversionError
-          message = "Skipping backup for '#{folder.name}' " \
-                    "as it is not UTF-7 encoded correctly"
-          Logger.logger.info message
-          next
-        end
-
-        Logger.logger.debug "[#{folder.name}] running backup"
-
-        serializer.apply_uid_validity(folder.uid_validity)
-
-        download_serializer =
-          case account.download_strategy
-          when "direct"
-            serializer
-          when "delay_metadata"
-            Serializer::DelayedMetadataSerializer.new(serializer: serializer)
-          else
-            raise "Unknown download strategy '#{account.download_strategy}'"
-          end
-
-        downloader = Downloader.new(
-          folder,
-          download_serializer,
-          multi_fetch_size: account.multi_fetch_size,
-          reset_seen_flags_after_fetch: account.reset_seen_flags_after_fetch
-        )
-        # rubocop:disable Lint/RescueException
-        download_serializer.transaction do
-          downloader.run
-        rescue Exception => e
-          message = <<~ERROR
-            #{self.class} error #{e}
-            #{e.backtrace.join("\n")}
-          ERROR
-          Logger.logger.error message
-          download_serializer.rollback
-          raise e
-        end
-        # rubocop:enable Lint/RescueException
-        if account.mirror_mode
-          Logger.logger.info "Mirror mode - Deleting messages only present locally"
-          LocalOnlyMessageDeleter.new(folder, serializer).run
-        end
-        FlagRefresher.new(folder, serializer).run if account.mirror_mode || refresh
+      backup_folders = Account::BackupFolders.new(
+        client: account.client, account: account
+      )
+      backup_folders.each do |folder|
+        backup_folder folder
       end
     end
 
     private
 
-    def each_folder
-      backup_folders = Account::BackupFolders.new(
-        client: account.client, account: account
-      )
-      backup_folders.each do |folder|
-        serializer = Serializer.new(account.local_path, folder.name)
-        yield folder, serializer
+    def backup_folder(folder)
+      serializer = Serializer.new(account.local_path, folder.name)
+      begin
+        return if !folder.exist?
+      rescue Encoding::UndefinedConversionError
+        message = "Skipping backup for '#{folder.name}' " \
+                  "as it is not UTF-7 encoded correctly"
+        Logger.logger.info message
+        return
       end
+
+      Logger.logger.debug "[#{folder.name}] running backup"
+
+      serializer.apply_uid_validity(folder.uid_validity)
+
+      download_serializer =
+        case account.download_strategy
+        when "direct"
+          serializer
+        when "delay_metadata"
+          Serializer::DelayedMetadataSerializer.new(serializer: serializer)
+        else
+          raise "Unknown download strategy '#{account.download_strategy}'"
+        end
+
+      downloader = Downloader.new(
+        folder,
+        download_serializer,
+        multi_fetch_size: account.multi_fetch_size,
+        reset_seen_flags_after_fetch: account.reset_seen_flags_after_fetch
+      )
+      # rubocop:disable Lint/RescueException
+      download_serializer.transaction do
+        downloader.run
+      rescue StandardError => e
+        message = <<~ERROR
+          #{self.class} error #{e}
+          #{e.backtrace.join("\n")}
+        ERROR
+        Logger.logger.error message
+        download_serializer.rollback
+        raise e
+      rescue SignalException => e
+        Logger.logger.error "#{self.class} handling #{e.class}"
+        download_serializer.rollback
+        raise e
+      end
+      # rubocop:enable Lint/RescueException
+      if account.mirror_mode
+        Logger.logger.info "Mirror mode - Deleting messages only present locally"
+        LocalOnlyMessageDeleter.new(folder, serializer).run
+      end
+      FlagRefresher.new(folder, serializer).run if account.mirror_mode || refresh
     end
   end
 end
