96bb4b64c52a562576a4600a24d97743a394d414
Joe Yates <joe.g.yates@gmail.com>
2023-09-25 10:09:05 +0200

    Add an on-error callback to retry mechanism

--- lib/retry_on_error.rb
+++ lib/retry_on_error.rb
@@ -1,11 +1,12 @@
 module RetryOnError
-  def retry_on_error(errors:, limit: 10)
+  def retry_on_error(errors:, limit: 10, on_error: nil)
     tries ||= 1
     yield
   rescue *errors => e
     if tries < limit
       message = "#{e}, attempt #{tries} of #{limit}"
       Imap::Backup::Logger.logger.debug message
+      on_error&.call
       tries += 1
       retry
     end
--- spec/unit/retry_on_error_spec.rb
+++ spec/unit/retry_on_error_spec.rb
@@ -3,12 +3,12 @@ class Retrier
 
   include RetryOnError
 
-  def do_stuff(errors:, limit:)
+  def do_stuff(errors:, limit: 10, on_error: nil)
     calls = 0
 
-    retry_on_error(errors: errors, limit: limit) do
+    retry_on_error(errors: errors, limit: limit, on_error: on_error) do
       calls += 1
-      raise FooError if calls < 3
+      raise FooError, "Failed!" if calls < 3
 
       42
     end
@@ -27,7 +27,25 @@ RSpec.describe RetryOnError do
       it "fails" do
         expect do
           subject.do_stuff(errors: [Retrier::FooError], limit: 1)
-        end.to raise_error(Retrier::FooError)
+        end.to raise_error(Retrier::FooError, /Failed/)
+      end
+    end
+
+    context "when unexpected errors are raised" do
+      it "fails" do
+        expect do
+          subject.do_stuff(errors: [RuntimeError])
+        end.to raise_error(Retrier::FooError, /Failed/)
+      end
+    end
+
+    context "when an :on_error block is passed" do
+      it "calls the block before retrying" do
+        on_error_calls = 0
+        error_proc = -> { on_error_calls += 1 }
+        subject.do_stuff(errors: [Retrier::FooError], on_error: error_proc)
+
+        expect(on_error_calls).to eq(2)
       end
     end
   end
