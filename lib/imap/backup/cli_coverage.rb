1e10c1d21cb9d6c9230dedecf393add3393de5c8
Joe Yates <joe.g.yates@gmail.com>
2023-10-13 09:24:10 +0200

    Move cli_coverage under imap/backup namespace

---
+++ lib/imap/backup/cli_coverage.rb
@@ -0,0 +1,21 @@
+module Imap; end
+
+module Imap::Backup
+  class CliCoverage
+    def self.conditionally_activate
+      return if !ENV.key?("COVERAGE")
+
+      # Collect coverage separately
+      ENV["SIMPLECOV_COMMAND_NAME"] = "#{ENV.fetch('COVERAGE')} #{ARGV.join(' ')} coverage"
+      require "simplecov"
+
+      # Silence output
+      SimpleCov.formatter = SimpleCov::Formatter::SimpleFormatter
+      SimpleCov.print_error_status = false
+
+      # Ensure SimpleCov doesn't filter out all out code
+      project_root = File.expand_path("..", __dir__)
+      SimpleCov.root project_root
+    end
+  end
+end
