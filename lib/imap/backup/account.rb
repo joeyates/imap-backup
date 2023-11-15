b925a5d36b218ff22b4e37cb5085bbd6b14e0a0c
Joe Yates <joe.g.yates@gmail.com>
2023-11-15 18:06:58 +0000

    Document Account attributes

--- lib/imap/backup/account.rb
+++ lib/imap/backup/account.rb
@@ -6,19 +6,62 @@ require "imap/backup/account/restore"
 module Imap; end
 
 module Imap::Backup
+  # Contains the attributes relating to an email account.
   class Account
+    # By default, the backup process fetches one email at a time
     DEFAULT_MULTI_FETCH_SIZE = 1
 
+    # The username of the account (usually the same as the email address)
+    # @return [String]
     attr_reader :username
+    # The password of the Account
     attr_reader :password
+    # The path where backups will be saved
+    # @return [String]
     attr_reader :local_path
+    # @overload folders
+    #   The list of folders that have been configured for the Account
+    #   @see #folder_blacklist how this list is interpreted.
+    #   @return [Array<String>, void]
+    # @overload folders=(value)
+    #   Sets the folders attribute and marks it as modified, storing the original value
+    #   @param value [Array<String>] a list of folders
+    #   @return [void]
     attr_reader :folders
+    # Indicates whether the configured folders are a whitelist or a blacklist.
+    # When true, any folders attribute will be taken as a list of folders to
+    # skip when running backups.
+    # When false, the folders attribute is used as the list of folders to backup.
+    # If no folders are configured, all folders on the server are backed up
+    # irrespective of the folder_blacklist setting
+    # @return [Boolean]
     attr_reader :folder_blacklist
+    # Should all emails be backed up progressively, or should emails
+    # which are deleted from the server be deleted locally?
     attr_reader :mirror_mode
+    # The address of the IMAP server
+    # @return [String]
     attr_reader :server
+    # Extra options to be passed to the IMAP server when connecting
+    # @return [Hash, void]
     attr_reader :connection_options
+    # The name of the download strategy to adopt during backups
+    # @return [String]
     attr_accessor :download_strategy
+    # Should 'Seen' flags be cached before fetchiong emails and
+    # rewritten to the server afterwards?
+    #
+    # Some IMAP providers, notably Apple Mail, set the '\Seen' flag
+    # on emails when they are fetched. By setting `:reset_seen_flags_after_fetch`,
+    # a workaround is activated which checks which emails are 'unseen' before
+    # and after the fetch, and removes the '\Seen' flag from those which have changed.
+    # As this check is susceptible to 'race conditions', i.e. when a different
+    # client sets the '\Seen' flag while imap-backup is fetching, it is best
+    # to only use it when required (i.e. for IMAP providers which always
+    # mark messages as '\Seen' when accessed).
+    # @return [Boolean]
     attr_reader :reset_seen_flags_after_fetch
+    # Tracks changes to the Account's attributes
     attr_reader :changes
 
     def initialize(options)
@@ -38,23 +81,39 @@ module Imap::Backup
       @marked_for_deletion = false
     end
 
+    # Initializes a client for the account's IMAP server
+    #
+    # @return [Account::Client::Default] the client
     def client
       @client ||= Account::ClientFactory.new(account: self).run
     end
 
+    # Returns the namespaces configured for the account on the IMAP server
+    #
+    # @return [Array<String>] the namespaces
     def namespaces
       client.namespace
     end
 
+    # Returns the capabilites of the IMAP server
+    #
+    # @return [Array<String>] the capabilities
     def capabilities
       client.capability
     end
 
+    # Restore the local backup to the server
+    #
+    # @return [void]
     def restore
       restore = Account::Restore.new(account: self)
       restore.run
     end
 
+    # Indicates whether the account has been configured, and is ready
+    # to be used
+    #
+    # @return [Boolean]
     def valid?
       username && password ? true : false
     end
@@ -63,18 +122,28 @@ module Imap::Backup
       changes.any?
     end
 
+    # Resets the store of changes, indicating that the current state is the saved state
     def clear_changes
       @changes = {}
     end
 
+    # Sets a flag indicating the Account should be excluded from the next save operation
+    #
+    # @return [void]
     def mark_for_deletion
       @marked_for_deletion = true
     end
 
+    # Indicates whether the account has been flagged for deletion during setup
+    #
+    # @return [Boolean]
     def marked_for_deletion?
       @marked_for_deletion
     end
 
+    # Returns all Account data for serialization
+    #
+    # @return [Hash]
     def to_h
       h = {username: @username, password: @password}
       h[:local_path] = @local_path if @local_path
@@ -90,14 +159,23 @@ module Imap::Backup
       h
     end
 
+    # Sets the username attribute and marks it as modified, storing the original value
+    #
+    # @return [void]
     def username=(value)
       update(:username, value)
     end
 
+    # Sets the password attribute and marks it as modified, storing the original value
+    #
+    # @return [void]
     def password=(value)
       update(:password, value)
     end
 
+    # Sets the local_path attribute and marks it as modified, storing the original value
+    #
+    # @return [void]
     def local_path=(value)
       update(:local_path, value)
     end
@@ -130,6 +208,9 @@ module Imap::Backup
       update(:connection_options, parsed)
     end
 
+    # The number of emails to fetch from the IMAP server at a time
+    #
+    # @return [Integer]
     def multi_fetch_size
       @multi_fetch_size ||= begin
         int = @multi_fetch_size_orignal.to_i
@@ -141,6 +222,10 @@ module Imap::Backup
       end
     end
 
+    # Sets the multi_fetch_size attribute and marks it as modified, storing the original value.
+    # If the supplied value is not a positive integer, uses {DEFAULT_MULTI_FETCH_SIZE}
+    #
+    # @return [void]
     def multi_fetch_size=(value)
       parsed = value.to_i
       parsed = DEFAULT_MULTI_FETCH_SIZE if !parsed.positive?
