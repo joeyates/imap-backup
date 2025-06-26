cb001d3c233e4edcb38d466a7360d147e4c69af2
Joe Yates <joe.g.yates@gmail.com>
2025-06-26 14:56:05 +0200

    Extract module

--- lib/imap/backup/cli.rb
+++ lib/imap/backup/cli.rb
@@ -12,6 +12,7 @@ module Imap::Backup
 
     autoload :Backup, "imap/backup/cli/backup"
     autoload :Local, "imap/backup/cli/local"
+    autoload :Migrate, "imap/backup/cli/migrate"
     autoload :Mirror, "imap/backup/cli/mirror"
     autoload :Remote, "imap/backup/cli/remote"
     autoload :Restore, "imap/backup/cli/restore"
@@ -139,68 +140,7 @@ module Imap::Backup
     desc "local SUBCOMMAND [OPTIONS]", "View local info"
     subcommand "local", Local
 
-    desc(
-      "migrate SOURCE_EMAIL DESTINATION_EMAIL [OPTIONS]",
-      "(Deprecated) Uploads backed-up emails from account SOURCE_EMAIL to account DESTINATION_EMAIL"
-    )
-    long_desc <<~DESC
-      This command is deprecated and will be removed in a future version.
-      Use 'copy' instead.
-
-      All emails which have been backed up for the "source account" (SOURCE_EMAIL) are
-      uploaded to the "destination account" (DESTINATION_EMAIL).
-
-      Some configuration may be necessary, as follows:
-
-      #{Helpers::NAMESPACE_CONFIGURATION_DESCRIPTION}
-
-      Finally, if you want to delete existing emails in destination folders,
-      use the `--reset` option. In this case, all existing emails are
-      deleted before uploading the migrated emails.
-    DESC
-    config_option
-    quiet_option
-    verbose_option
-    method_option(
-      "automatic-namespaces",
-      type: :boolean,
-      desc: "automatically choose delimiters and prefixes"
-    )
-    method_option(
-      "destination-delimiter",
-      type: :string,
-      desc: "the delimiter for destination folder names"
-    )
-    method_option(
-      "destination-prefix",
-      type: :string,
-      desc: "the prefix (namespace) to add to destination folder names",
-      aliases: ["-d"]
-    )
-    method_option(
-      "reset",
-      type: :boolean,
-      desc: "DANGER! This option deletes all messages from destination folders before uploading",
-      aliases: ["-r"]
-    )
-    method_option(
-      "source-delimiter",
-      type: :string,
-      desc: "the delimiter for source folder names"
-    )
-    method_option(
-      "source-prefix",
-      type: :string,
-      desc: "the prefix (namespace) to strip from source folder names",
-      aliases: ["-s"]
-    )
-    # Migrates emails from one account to another
-    # @return [void]
-    def migrate(source_email, destination_email)
-      non_logging_options = Imap::Backup::Logger.setup_logging(options)
-      Transfer.new(:migrate, source_email, destination_email, non_logging_options).run
-    end
-
+    include Migrate
     include Mirror
 
     desc "remote SUBCOMMAND [OPTIONS]", "View info about online accounts"
