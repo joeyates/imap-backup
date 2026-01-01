39c2ab5fce6a918c2a57933555ffcb7534db3b75
Joe Yates <joe.g.yates@gmail.com>
2026-01-01 16:05:50 +0100

    Implement Account locking

---
+++ lib/imap/backup/account/locker.rb
@@ -0,0 +1,35 @@
+require "imap/backup/account/folder_ensurer"
+require "imap/backup/lockfile"
+
+module Imap; end
+
+module Imap::Backup
+  class Account; end
+
+  class Account::Locker
+    attr_reader :account
+
+    def initialize(account:)
+      @account = account
+    end
+
+    def with_lock(&block)
+      lockfile = Lockfile.new(path: account.lockfile_path)
+      if lockfile.exists?
+        if !lockfile.stale?
+          raise Lockfile::LockfileExistsError,
+                "Lockfile '#{account.lockfile_path}' exists and is not stale."
+        end
+
+        Logger.logger.info("Stale lockfile '#{account.lockfile_path}' found. Removing it.")
+        lockfile.remove
+      else
+        Account::FolderEnsurer.new(account: account).run
+      end
+
+      lockfile.with_lock do
+        block.call
+      end
+    end
+  end
+end
---
+++ spec/unit/account/locker_spec.rb
@@ -0,0 +1,93 @@
+require "imap/backup/account/locker"
+require "imap/backup/account"
+require "imap/backup/lockfile"
+
+module Imap
+  module Backup
+    RSpec.describe Account::Locker do
+      subject { described_class.new(account: account) }
+
+      let(:lockfile_path) { "lockfile_path" }
+      let(:account) do
+        instance_double(Account, lockfile_path: lockfile_path, local_path: "local_path")
+      end
+      let(:lockfile) { instance_double(Lockfile, exists?: false) }
+
+      before do
+        allow(Imap::Backup::Lockfile).to receive(:new).with(path: lockfile_path) { lockfile }
+      end
+
+      describe "#initialize" do
+        it "sets the account" do
+          expect(subject.account).to eq(account)
+        end
+      end
+
+      describe "#with_lock" do
+        it "creates a Lockfile" do
+          expect(lockfile).to receive(:with_lock).and_yield
+
+          subject.with_lock {}
+        end
+
+        it "calls the supplied the block" do
+          allow(lockfile).to receive(:with_lock).and_yield
+
+          yielded = false
+          subject.with_lock { yielded = true }
+
+          expect(yielded).to be true
+        end
+
+        context "when the lockfile exists and is stale" do
+          before do
+            allow(lockfile).to receive(:exists?) { true }
+            allow(lockfile).to receive(:stale?) { true }
+          end
+
+          it "removes the lockfile" do
+            expect(lockfile).to receive(:remove)
+
+            allow(lockfile).to receive(:with_lock).and_yield
+
+            subject.with_lock {}
+          end
+
+          it "logs the removal of the stale lockfile" do
+            expect(Logger.logger).to receive(:info).with(
+              "Stale lockfile '#{account.lockfile_path}' found. Removing it."
+            )
+
+            allow(lockfile).to receive(:remove)
+            allow(lockfile).to receive(:with_lock).and_yield
+
+            subject.with_lock {}
+          end
+
+          it "calls the supplied the block" do
+            allow(lockfile).to receive(:remove)
+            allow(lockfile).to receive(:with_lock).and_yield
+
+            yielded = false
+            subject.with_lock { yielded = true }
+
+            expect(yielded).to be true
+          end
+        end
+
+        context "when the lockfile exists and is not stale" do
+          before do
+            allow(lockfile).to receive(:exists?) { true }
+            allow(lockfile).to receive(:stale?) { false }
+          end
+
+          it "raises an error" do
+            expect do
+              subject.with_lock {}
+            end.to raise_error("Lockfile '#{account.lockfile_path}' exists and is not stale.")
+          end
+        end
+      end
+    end
+  end
+end
