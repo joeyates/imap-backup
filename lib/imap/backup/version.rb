a1c567806c24495ce09b2955fe5619bab019a9ba
Joe Yates <joe.g.yates@gmail.com>
2026-03-30 16:29:45 +0200

    Prepare release

--- lib/imap/backup/version.rb
+++ lib/imap/backup/version.rb
@@ -2,13 +2,13 @@ module Imap; end
 
 module Imap::Backup
   # @private
-  MAJOR    = 16
+  MAJOR    = 17
   # @private
-  MINOR    = 6
+  MINOR    = 0
   # @private
   REVISION = 0
   # @private
-  PRE      = nil
+  PRE      = "rc0"
   # The application version
   VERSION  = [MAJOR, MINOR, REVISION, PRE].compact.map(&:to_s).join(".")
 end
